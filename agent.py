90369ee43d9f3951d5fb62294353baf217a0b19b
Shreyas010705 <shreyaskumarmusic@gmail.com>
2026-05-04 11:42:41 +0530

    Refactor QLearningAgent class and methods

--- agent.py
+++ agent.py
@@ -5,37 +5,25 @@ class QLearningAgent:
     def __init__(self):
         self.q_table = {}
 
-        # Tuned for stability
-        self.alpha = 0.1      # learning rate
-        self.gamma = 0.9      # future reward importance
-        self.epsilon = 0.1    # exploration
+        self.alpha = 0.1
+        self.gamma = 0.9
+        self.epsilon = 0.1
 
-        self.actions = [0, 1]  # 0 = wait, 1 = transmit
+        self.actions = [0, 1]
 
-    # -------------------------
-    # Get Q value
-    # -------------------------
     def get_q(self, state, action):
         return self.q_table.get((state, action), 0.0)
 
-    # -------------------------
-    # Choose action
-    # -------------------------
     def choose_action(self, state):
         if random.random() < self.epsilon:
             return random.choice(self.actions)
 
-        q_values = [self.get_q(state, a) for a in self.actions]
-        return self.actions[q_values.index(max(q_values))]
+        q_vals = [self.get_q(state, a) for a in self.actions]
+        return self.actions[q_vals.index(max(q_vals))]
 
-    # -------------------------
-    # Update Q value
-    # -------------------------
     def update(self, state, action, reward, next_state):
         old_q = self.get_q(state, action)
-
         future_q = max([self.get_q(next_state, a) for a in self.actions])
 
         new_q = old_q + self.alpha * (reward + self.gamma * future_q - old_q)
-
         self.q_table[(state, action)] = new_q
