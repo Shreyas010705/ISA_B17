6c9cf4ed347aea06bbbc792565ef42c385cb1e9b
Shreyas010705 <shreyaskumarmusic@gmail.com>
2026-05-04 11:43:07 +0530

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -1,37 +1,35 @@
 import random
 
 
-# -------------------------
-# Greedy Policy
-# -------------------------
 class GreedyPolicy:
     def choose_action(self, state):
-        aoi, battery, _ = state
+        aoi, battery, sleep = state
+
+        if sleep == 1:
+            return 0
 
         if battery > 0:
-            return 1  # try to transmit
+            return 1
         return 0
 
 
-# -------------------------
-# Periodic Policy
-# -------------------------
 class PeriodicPolicy:
     def __init__(self, interval=2):
         self.interval = interval
         self.counter = 0
 
     def choose_action(self, state):
-        self.counter += 1
+        _, _, sleep = state
 
+        if sleep == 1:
+            return 0
+
+        self.counter += 1
         if self.counter % self.interval == 0:
             return 1
         return 0
 
 
-# -------------------------
-# Random Policy (optional)
-# -------------------------
 class RandomPolicy:
     def choose_action(self, state):
         return random.choice([0, 1])
